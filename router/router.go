53fa9d3d1ed4549bfeb0b0ce8a8c6c06d7f22e0a
mojie <mojie@flamingo-inc.com>
2020-06-21 20:35:31 +0800

    add views

--- router/router.go
+++ router/router.go
@@ -1,7 +1,11 @@
 package router
 
 import (
+	"fmt"
+	"html/template"
 	"iv-go/apis"
+	"net/http"
+	"time"
 
 	"iv-go/pkg"
 
@@ -11,6 +15,10 @@ import (
 // SetupRouter 路由设置
 func SetupRouter() *gin.Engine {
 	router := gin.New()
+
+	// web页面路由
+	pageGroup(router)
+
 	router.GET("/welcome", apis.Welcome)
 
 	user := router.Group("/users")
@@ -31,4 +39,30 @@ func SetupRouter() *gin.Engine {
 	}
 
 	return router
+}
+
+func pageGroup(r *gin.Engine) *gin.Engine {
+	r.Delims("{[{", "}]}")
+	r.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
+
+	r.LoadHTMLGlob("views/**/*")
+
+	page := r.Group("/page", pkg.EnableCookieSession())
+	{
+		page.StaticFS("/assets", http.Dir("assets"))
+		page.GET("/raw", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "raw.tmpl", map[string]interface{}{
+				"now": time.Date(2020, 06, 21, 0, 0, 0, 0, time.UTC),
+			})
+		})
+	}
+
+	return r
+}
+
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d/%02d/%02d", year, month, day)
 }
