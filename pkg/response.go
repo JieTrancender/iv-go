d7b21e200fe1d3daedcfb97f2fda7a596f96fbfd
mojie <mojie@flamingo-inc.com>
2020-06-21 19:27:53 +0800

    封装JSON Response

---
+++ pkg/response.go
@@ -0,0 +1,14 @@
+package pkg
+
+import "github.com/gin-gonic/gin"
+
+// RespJSON for resp json data
+func RespJSON(c *gin.Context, code int, message string, content interface{}) {
+	c.JSON(code, gin.H{
+		"code":    code,
+		"message": "message",
+		"data": gin.H{
+			"content": content,
+		},
+	})
+}
